4b928c2625092daaf262af13a6cccf620e0d6a88
unknown <dimiter_georgiev@mail.bg>
2021-09-15 13:10:24 -0700

    added start message to tickets service

--- ticket/src/index.ts
+++ ticket/src/index.ts
@@ -6,6 +6,7 @@ import {OrderCreatedListener} from './events/listeners/order-created-listener';
 import {OrderCancelledListener} from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
+    console.log('Starting...');
     if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined.');
     }
